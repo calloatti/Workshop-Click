0ec374d2045045100f3f4db703a769913c8f6595
Carlos Alloatti <calloatti@gmail.com>
2026-08-19 19:10:45 -0300

    Update 15 files
    2026-08-19 19:10

--- Version-1.0/Source/WorkshopClickConfigurator.cs
+++ Version-1.0/Source/WorkshopClickConfigurator.cs
@@ -1,28 +1,9 @@
-﻿using Bindito.Core;
-using Calloatti.Config;
-using HarmonyLib;
+using Bindito.Core;
 using Timberborn.AssetSystem;
-using Timberborn.ModManagerScene;
 using Timberborn.SingletonSystem;
-using UnityEngine;
 
 namespace Calloatti.WorkshopClick
 {
-  public partial class WorkshopClickStarter : IModStarter
-  {
-    public static SimpleConfig Config { get; private set; }
-
-    public void StartMod(IModEnvironment modEnvironment)
-    {
-      Config = new SimpleConfig(modEnvironment.ModPath);
-
-      var harmony = new Harmony("com.calloatti.workshopclick");
-      harmony.PatchAll();
-
-      Debug.Log("[Calloatti.WorkshopClick] Core Initialized.");
-    }
-  }
-
   [Context("MainMenu")]
   public class WorkshopClickConfigurator : Configurator
   {
---
+++ Version-1.0/rebuild.cmd
@@ -0,0 +1,5 @@
+@echo off
+echo Rebuilding Workshop Click (Version-1.0)...
+cd /d "C:\Users\calloatti\source\repos\Mods\Workshop Click\Version-1.0"
+dotnet build "Workshop Click.csproj" -c Release -t:Rebuild /nodeReuse:false
+pause
